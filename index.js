36d81b1dda0b0d57753e8f9442a192e1182fc6b1
Anant Sinha <anantsinha9811@gmail.com>
2018-09-27 22:36:01 +0530

    Parse request, get payload

--- index.js
+++ index.js
@@ -4,11 +4,38 @@
 */
 
 //Dependencies
-const http = require('http')
-
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 // The server should respond to all requests with a string
 let server = http.createServer(function (req,res){
-  res.end('Hello World\n');
+  //Get the url and parse it
+  let parsedUrl = url.parse(req.url, true);
+  //Get the path
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  //Get the HTTP method
+  let method = req.method.toLowerCase();
+  //Get the headers
+  let headers = req.headers;
+  let i = 0;
+  //Get the payload as an object
+  let decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data',data => {
+    i++;
+    buffer+=decoder.write(data);
+  });
+
+  req.on('end',()=>{
+    buffer+=decoder.end();
+      //Log the request path
+      console.log('Payload: ',buffer+ ' came in ' + i + ' pieces');
+    //Get the query string as an object
+    let queryStringObject = parsedUrl.query;
+    //Send the response
+    res.end('Hello World\n');
+  })
 });
 
 //Start the server, and have it listen on port 3000
